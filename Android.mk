aefc0b5b9c26e9e58f6cf689c0bb4a4cf4e0490f
The Android Open Source Project <initial-contribution@android.com>
2008-12-17 18:06:04 -0800

    Code drop from //branches/cupcake/...@124589

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := user development
 
 contacts_provider_files := ../ContactsProvider/src/com/android/providers/contacts/ContactsProvider.java
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files) $(contacts_provider_files)
+LOCAL_SRC_FILES := $(call all-java-files-under,src) $(contacts_provider_files)
 
 LOCAL_JAVA_LIBRARIES := ext
 
@@ -18,3 +18,5 @@ LOCAL_CERTIFICATE := shared
 LOCAL_OVERRIDES_PACKAGES := ContactsProvider
 
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
